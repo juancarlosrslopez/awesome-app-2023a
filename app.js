c65906cd9368028aa5e2ef44f44c5d32cc6fdae2
juancarlos.rslopez <juancarlos.rslopez@gmail.com>
2023-05-12 15:44:16 +0000

    📝 Class12May

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// Importar el modulo http
-import http from 'http';
+/*Importar el modulo http
+import http from 'http';*/
 
 // Importando expressjs
 import express from 'express';
@@ -9,7 +9,7 @@ const app = express(); // (req, res)=>{...}
 
 // Registrar nuestro primer middleware
 app.use((req, res, next)=>{
-    console.log("📣 Ejecutando el Middleware 1");
+    console.log("\n📣 Ejecutando el Middleware 1");
     next();
 });
 
@@ -23,19 +23,19 @@ app.use((req, res)=>{
     console.log("⭐ Respondiendo al cliente");
     res.send(`
     <h1>Welcome to express</h1>
-    <p>This is my awesome app</p>
+    <p>Este es mi software</p>
     `);
 });
 
-// Crear el servidor 
-const server = http.createServer(app);
+/*Crear el servidor 
+const server = http.createServer(app);*/
 
 // Definir puertos 
 const port = 3000; //5500
 const ip = "0.0.0.0"; //127.0.0.1
 
 // Arrancando el server
-server.listen(port, ip, ( err )=>{
+app.listen(port, ip, ( err )=>{
     console.log("📣 Sirviendo en https://localhost:3000");
     console.log(`📣 Sirviendo en https://${process.env.IP}:${process.env.PORT}`);
 })
