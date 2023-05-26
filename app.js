9b5044cfe140394cd78fa1a8d2e3241e27814606
juancarlos.rslopez <juancarlos.rslopez@gmail.com>
2023-05-26 15:49:25 +0000

    📣 Servidor de archivos estaticos

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 // Importando Express
 import express from 'express';
 // Importando http-status
-//import httpStatus from 'http-status';
+import httpStatus from 'http-status';
+import path from 'path';
 
 // Importando el enrutador
 import adminRouter from './routes/admin.route.js';
 import shopRouter from './routes/shop.route.js';
 import NotFoundRouter from './routes/NotFound.route.js';
 
+// Importando ROOT_DIR
+import { ROOT_DIR } from './helpers/paths.js';
+
+
 // Creando la instancia de express
 // que basicamente es un middleware
 const app = express();
+
+// Se registra el middleware para
+// Servidor de archivos estaticos
+app.use(express.static(path.join(ROOT_DIR,'public')));
 
 // Se registra el middleware del body-parser
 app.use(express.urlencoded({ extended: true }));
