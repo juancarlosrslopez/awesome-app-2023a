2decf75d203e39e10e911ccf91e2d284cb69370d
juancarlos.rslopez <juancarlos.rslopez@gmail.com>
2023-05-29 17:43:18 +0000

    🛢Modelo de Datos

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -2,25 +2,29 @@
 import { Router } from 'express';
 // Importando el gestor de rutas
 import path from 'path';
-// Importando ROOT_DIR
 import { ROOT_DIR } from '../helpers/paths.js';
 
 // Creando una instancia del enrutador de express
 const router = Router();
 
+// Datos en memoria volatil
+export const products = [];
+
 // GET /admin/add-product
 router.get('/add-product', (req, res, next) => {
   // Servimos el formulario
   console.log("📢 Sirviendo formulario...");
-  console.log(`ROOT_DIR: ${ROOT_DIR}`);
   res.sendFile(path.join(ROOT_DIR, 'views','add-product.html'));
 });
 
 // POST /admin/add-product
 router.post('/add-product', (req, res) => {
-  // Realizaremos la extracción de
-  // parametros dentro de la peticion
-  console.log(req.body);
+  // Realizaremos la desestructuracion de
+  // "name" de la petición
+  const { title } = req.body;
+  // Agregamos el dato en la base de datos
+  products.push(title);
+  // Redireccionando
   res.redirect('/');
 });
 
